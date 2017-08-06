6477d44995e4e8ce70189921e540206ea74202dd
AN AN <2236368544@qq.com>
2017-08-07 00:41:11 +0800

    Add  `AAQRCodeModel`  files

---
+++ AAQRCodeSugarDemo/AAQRCodeSugarDemo/AAQRCodeModel.h
@@ -0,0 +1,13 @@
+//
+//  AAQRCodeModel.h
+//  AAQRCodeSugarDemo
+//
+//  Created by 金银岛 on 2017/8/7.
+//  Copyright © 2017年 An An. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AAQRCodeModel : NSObject
+
+@end
---
+++ AAQRCodeSugarDemo/AAQRCodeSugarDemo/AAQRCodeModel.m
@@ -0,0 +1,13 @@
+//
+//  AAQRCodeModel.m
+//  AAQRCodeSugarDemo
+//
+//  Created by 金银岛 on 2017/8/7.
+//  Copyright © 2017年 An An. All rights reserved.
+//
+
+#import "AAQRCodeModel.h"
+
+@implementation AAQRCodeModel
+
+@end
