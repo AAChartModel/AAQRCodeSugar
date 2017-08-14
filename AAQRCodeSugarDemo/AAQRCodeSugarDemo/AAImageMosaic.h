d3685f8d32cbe7e3fe0f66ca955872fbdd50b9d4
李照闫 <2236368544@qq.com>
2017-08-14 19:26:00 +0800

    Add `AAImageMosaic` files

---
+++ AAQRCodeSugarDemo/AAQRCodeSugarDemo/AAImageMosaic.h
@@ -0,0 +1,13 @@
+//
+//  AAImageMosaic.h
+//  AAQRCodeSugarDemo
+//
+//  Created by An An on 2017/8/14.
+//  Copyright © 2017年 An An. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AAImageMosaic : NSObject
+
+@end
---
+++ AAQRCodeSugarDemo/AAQRCodeSugarDemo/AAImageMosaic.m
@@ -0,0 +1,13 @@
+//
+//  AAImageMosaic.m
+//  AAQRCodeSugarDemo
+//
+//  Created by An An on 2017/8/14.
+//  Copyright © 2017年 An An. All rights reserved.
+//
+
+#import "AAImageMosaic.h"
+
+@implementation AAImageMosaic
+
+@end
