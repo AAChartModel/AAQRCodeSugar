feceedc7000a0b1ab5f48c1a7aa820cf8e44a8ad
李照闫 <2236368544@qq.com>
2017-09-19 16:56:09 +0800

    Add `CHINESE-README.md`

--- AAQRCodeSugarDemo/AAQRCodeSugarDemo/ViewController.m
+++ AAQRCodeSugarDemo/AAQRCodeSugarDemo/ViewController.m
@@ -10,30 +10,185 @@
 #import "AAImageMosaic.h"
 @interface ViewController ()
 
+@property (nonatomic, strong) UIImageView *imageView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIImageView *imageView = [[UIImageView alloc]init];
-    imageView.backgroundColor = [UIColor cyanColor];
-    imageView.frame = CGRectMake(50, 100,200, 200);
     
-//    UIImage *originalImage = [UIImage imageNamed:@"16357599.jpeg"];
+    self.view.backgroundColor = [UIColor redColor];
+    self.imageView = [[UIImageView alloc]init];
+    self.imageView.backgroundColor = [UIColor cyanColor];
+    self.imageView.frame = CGRectMake(50, 100,200, 200);
+    
+    //    UIImage *originalImage = [UIImage imageNamed:@"16357599.jpeg"];
     UIImage *originalImage = [UIImage imageNamed:@"IMG_2105.JPG"];
     UIImage *finalImage = [AAImageMosaic grayImage:originalImage];
-    imageView.image = [AAImageMosaic convertToGrayscale:finalImage];
+    self.imageView.image = [AAImageMosaic convertToGrayscale:finalImage];
     
-    [self.view addSubview:imageView];
-
+    self.imageView.image = [self imageWithImage:self.imageView.image scaledToSize:CGSizeMake(30, 30)];
+//    self.imageView.image = [self imageToTransparent:self.imageView.image];
+    
+    [self.view addSubview: self.imageView];
+//    [self drawImage];
  }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//图片压缩
+- (UIImage *)imageWithImage:(UIImage*)image
+               scaledToSize:(CGSize)newSize;
+{
+    UIGraphicsBeginImageContext(newSize);
+    [image drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+    
 }
 
 
+- (void)drawImage
+{
+    CGImageRef cgimage = [self.imageView.image CGImage];
+    
+    size_t width = CGImageGetWidth(cgimage); // 图片宽度
+    size_t height = CGImageGetHeight(cgimage); // 图片高度
+    unsigned char *data = calloc(width * height * 4, sizeof(unsigned char)); // 取图片首地址
+    size_t bitsPerComponent = 8; // r g b a 每个component bits数目
+    size_t bytesPerRow = width * 4; // 一张图片每行字节数目 (每个像素点包含r g b a 四个字节)
+    CGColorSpaceRef space = CGColorSpaceCreateDeviceRGB(); // 创建rgb颜色空间
+    
+    CGContextRef context =CGBitmapContextCreate(data,
+                                                width,
+                                                height,
+                                                bitsPerComponent,
+                                                bytesPerRow,
+                                                space,
+                                                kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), cgimage);
+    
+    for (size_t i = 0; i < height; i++)
+    {
+        for (size_t j = 0; j < width; j++)
+        {
+            size_t pixelIndex = i * width * 4 + j * 4;
+            
+            unsigned char red = data[pixelIndex];
+            unsigned char green = data[pixelIndex + 1];
+            unsigned char blue = data[pixelIndex + 2];
+            
+            // 修改颜色
+            red = 0;
+            data[pixelIndex] = red;
+            data[pixelIndex] = green;
+            data[pixelIndex] = blue;
+        }
+    }
+    
+    cgimage = CGBitmapContextCreateImage(context);
+    self.imageView.image = [UIImage imageWithCGImage:cgimage];
+}
+
+
+//去除图片的白色背景
+
+- (UIImage*) imageToTransparent:(UIImage*) image
+
+{
+    
+    // 分配内存
+    
+    const int imageWidth = image.size.width;
+    
+    const int imageHeight = image.size.height;
+    
+    size_t bytesPerRow = imageWidth * 4;
+    
+    uint32_t* rgbImageBuf = (uint32_t*)malloc(bytesPerRow * imageHeight);
+    
+    
+    
+    // 创建context
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    CGContextRef context = CGBitmapContextCreate(rgbImageBuf, imageWidth, imageHeight, 8, bytesPerRow, colorSpace,
+                                                 
+                                                 kCGBitmapByteOrder32Little | kCGImageAlphaNoneSkipLast);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, imageWidth, imageHeight), image.CGImage);
+    
+    
+    
+    // 遍历像素
+    
+    int pixelNum = imageWidth * imageHeight;
+    
+    uint32_t* pCurPtr = rgbImageBuf;
+    
+    for (int i = 0; i < pixelNum; i++, pCurPtr++)
+        
+    {
+        
+        //        //去除白色...将0xFFFFFF00换成其它颜色也可以替换其他颜色。
+        
+        //        if ((*pCurPtr & 0xFFFFFF00) >= 0xffffff00) {
+        
+        //
+        
+        //            uint8_t* ptr = (uint8_t*)pCurPtr;
+        
+        //            ptr[0] = 0;
+        
+        //        }
+        
+        //接近白色
+        
+        //将像素点转成子节数组来表示---第一个表示透明度即ARGB这种表示方式。ptr[0]:透明度,ptr[1]:R,ptr[2]:G,ptr[3]:B
+        
+        //分别取出RGB值后。进行判断需不需要设成透明。
+        
+        uint8_t* ptr = (uint8_t*)pCurPtr;
+        
+        if (ptr[1] > 240 && ptr[2] > 240 && ptr[3] > 240) {
+            
+            //当RGB值都大于240则比较接近白色的都将透明度设为0.-----即接近白色的都设置为透明。某些白色背景具有杂质就会去不干净，用这个方法可以去干净
+            
+            ptr[0] = 0;
+            
+        }
+        
+    }
+    
+    // 将内存转成image
+    
+    CGDataProviderRef dataProvider =CGDataProviderCreateWithData(NULL, rgbImageBuf, bytesPerRow * imageHeight, nil);
+    
+    
+    
+    CGImageRef imageRef = CGImageCreate(imageWidth, imageHeight,8, 32, bytesPerRow, colorSpace,
+                                        
+                                        kCGImageAlphaLast |kCGBitmapByteOrder32Little, dataProvider,
+                                        
+                                        NULL, true,kCGRenderingIntentDefault);
+    
+    CGDataProviderRelease(dataProvider);
+    
+    UIImage* resultUIImage = [UIImage imageWithCGImage:imageRef];
+    
+    // 释放
+    
+    CGImageRelease(imageRef);
+    
+    CGContextRelease(context);
+    
+    CGColorSpaceRelease(colorSpace);
+    
+    return resultUIImage;
+    
+}
 @end
